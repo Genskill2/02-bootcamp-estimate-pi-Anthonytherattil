bc1ad02924e98405cdcd1dfa849e9fda1a4adb2e
Anthony Therattil <anthonytherattil@gmail.com>
2021-06-01 22:48:14 +0530

    Adds another sol

--- estimate.py
+++ estimate.py
@@ -32,23 +32,25 @@ class TestMC(unittest.TestCase):
 
      
 def wallis(n):
-    x=2
-    for k in range(1,n+1):
-        x*=(4*k*k)/((4*k*k)-1)
-        
+    x=1
+    for i in range (1,n+1):
+        a=(4*i*i)/(4*i*i-1)
+        x=x*a
+    x=x*2
     return x
 
 def monte_carlo(n):
-    ps=0
-    pc=0
-    for k in range(0,n):
-        x=random.random()
-        y=random.random()
-        ps+=1
-        if ((x*x)+(y*y))<=1:
-            pc+=1
-
-    return 4*(pc)/(ps)
+    import random
+    number_of_times=0
+    for i in range(1,n+1):
+        x = random.random()
+        y = random.random()
+        distance = (x * x + y * y) ** .5
+        if distance <= 1:
+            number_of_times = number_of_times + 1
+    a=number_of_times/n
+    a=a*4
+    return a
             
           
 if __name__ == "__main__":
